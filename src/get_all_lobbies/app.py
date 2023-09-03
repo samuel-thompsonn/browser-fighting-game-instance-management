280ba9b19130444d41d3d36cf4af06de1f1f0d2d
Samuel Thompson <sam.thompson356@gmail.com>
2023-09-03 01:51:45 -0400

    allow get_all_lobbies to read endpoint

--- src/get_all_lobbies/app.py
+++ src/get_all_lobbies/app.py
@@ -29,33 +29,12 @@ def lambda_handler(event, context):
     if table_name is None:
         raise RuntimeError("No target DDB table found!")
     
-    # TODO: Find a better way to assign non-clashing IDs
-    item_id = random.randrange(100000000)
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+    items = table.scan()
 
-    # dynamodb = boto3.resource('dynamodb')
-    # table = dynamodb.Table(table_name)
-    # table.put_item(
-    #     Item={
-    #         'ID': f"{item_id}"
-    #     }
-    # )
-
-    response_data = [
-        {
-            "id": 3,
-            "name": "Placeholder lobby name",
-            "status": "Waiting for players",
-            "playerCount": 1,
-            "maxPlayerCount": 2
-        },
-        {
-            "id": 7,
-            "name": "Placeholder lobby name 2",
-            "status": "In-Game",
-            "playerCount": 2,
-            "maxPlayerCount": 2
-        }
-    ]
+    print(items)
+    response_data = items['Items']
 
     return {
         "statusCode": 200,
@@ -63,5 +42,5 @@ def lambda_handler(event, context):
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        "body": json.dumps(response_data),
+        "body": json.dumps(response_data, default=str),
     }
